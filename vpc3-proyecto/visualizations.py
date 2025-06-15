86c5688b5cba122da798a0e9add69866da24f404
qagustina <agustinaqr@gmail.com>
2025-06-15 17:20:24 -0300

    add funcs

--- vpc3-proyecto/visualizations.py
+++ vpc3-proyecto/visualizations.py
@@ -2,29 +2,9 @@ import os
 import json
 import zipfile
 from collections import defaultdict
-from PIL import Image
+from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
-
-
-def load_anns(zip_path: str, json_file: str):
-    with zipfile.ZipFile(zip_path, 'r') as zipf:
-        with zipf.open(json_file) as file:
-            return json.load(file)
-
-
-def get_id(coco_dict: dict, subset: str = 'train'):
-    return {
-        int(k): v['file_name']
-        for k, v in coco_dict['imgs'].items()
-        if v['set'] == subset
-    }
-
-
-def group_anns(coco_dict: dict):
-    anns = defaultdict(list)
-    for ann in coco_dict['anns'].values():
-        anns[ann['image_id']].append(ann)
-    return anns
+from eda import load_anns, group_anns, get_imgs, get_id
 
 
 def get_imgs(id_to_filename: dict, img_folder: str, cant: int = 10):
@@ -38,7 +18,7 @@ def get_imgs(id_to_filename: dict, img_folder: str, cant: int = 10):
     return img_list
 
 
-def plot(img_list: list, anotaciones_por_imagen: dict, nombre_subset: str):
+def plot_examples(img_list: list, anotaciones_por_imagen: dict, nombre_subset: str, output_folder: str):
     fig, axs = plt.subplots(2, 5, figsize=(20, 12))
     axs = axs.flatten()
 
@@ -58,26 +38,55 @@ def plot(img_list: list, anotaciones_por_imagen: dict, nombre_subset: str):
     fig.suptitle(f"Images and annotations from the set {nombre_subset} examples.", fontsize=12)
     plt.tight_layout() 
 
-    # make dir 
-    output_folder = os.path.join('../reports/figures')
     os.makedirs(output_folder, exist_ok=True)
-
-    # output
     output_path = os.path.join(output_folder, f'visualization_{nombre_subset}.png')
     plt.savefig(output_path)
 
+def plot_class_examples(anns_imgs, id_to_filename, img_folder, img_class, output_path):
+    img_ok = False
+    for img_id, anns in anns_imgs.items():
+        if any(ann['class'] == img_class for ann in anns):
+            img_ok = True
+            filename = id_to_filename.get(img_id)
+            if not filename:
+                continue
+
+            ruta = os.path.join(img_folder, filename.split('/')[-1])
+            if not os.path.exists(ruta):
+                continue
+
+            image = Image.open(ruta).convert('RGB')
+            draw = ImageDraw.Draw(image)
+
+            # boxes
+            for ann in anns:
+                if ann['class'] == img_class: 
+                    x, y, w, h = ann['bbox']
+                    draw.rectangle([x, y, x + w, y + h], outline='red', width=2)
+
+            output_file = os.path.join(output_path, f'example_{img_class}.jpg')
+            image.save(output_file)
+            break  
+    if not img_ok:
+        print(f"No se encontró ninguna imagen con anotaciones {img_class}.")
 
 def main(subset_folder: str):
     zip_path = '../data/raw/cocotext.v2.zip'
     json_file = 'cocotext.v2.json'
     img_folder = os.path.join('../data/raw/subset', subset_folder)
+    output_path = '../reports/figures'
 
     coco_text = load_anns(zip_path, json_file)
     anns_imgs = group_anns(coco_text)
     id_to_filename = get_id(coco_text, subset='train')
 
     img_list = get_imgs(id_to_filename, img_folder, cant=10)
-    plot(img_list, anns_imgs, nombre_subset=subset_folder)
+    plot_examples(img_list, anns_imgs, nombre_subset=subset_folder, output_folder=output_path)
+    
+    # classes 
+    clases_objetivo = ['handwritten', 'machine printed']
+    for clase in clases_objetivo:
+        plot_class_examples(anns_imgs, id_to_filename, img_folder, clase, output_path)
 
 
 if __name__ == "__main__":
